22de8deb2da14ab181d3b147d42f80545265e242
forafish <fora.luo@gmail.com>
2017-06-09 15:48:36 +0800

    stats pub/sub

--- src/stats.js
+++ src/stats.js
@@ -10,50 +10,60 @@ export let attributes = {
   version: require('../package.json').version
 };
 
+function pubProcessInfo(trans, topic, interval) {
+  const info = {
+    app: trans.config.name,
+    eventLoopDelay: trans.load.eventLoopDelay,
+    heapUsed: trans.load.heapUsed,
+    rss: trans.load.rss,
+    nodeEnv: process.env.NODE_ENV,
+    uptime: process.uptime(),
+    ts: Date.now()
+  };
+
+  //debug('processInfo', info);
+  trans.act({
+    pubsub$: true,
+    topic,
+    cmd: 'processInfo',
+    info
+  });
+
+  setTimeout(pubProcessInfo.bind(null, trans, topic, interval), interval);
+}
+
+function pubActionsInfo(trans, topic, interval) {
+  const info = {
+    app: trans.config.name
+  };
+  const list = trans.list();
+  info.ts = Date.now();
+  info.actions = list.map(action => {
+    const schema = {};
+    return {
+      pattern: action.pattern,
+      plugin: action.plugin.attributes.name
+    };
+  });
+
+  //debug('actionsInfo', info);
+  trans.act({
+    pubsub$: true,
+    topic,
+    cmd: 'actionsInfo',
+    info
+  });
+
+  setTimeout(pubActionsInfo.bind(null, trans, topic, interval), interval);
+}
+
 export function plugin (options, next) {
   const trans = this;
   const topic = exports.attributes.name;
+  const sampleInterval = options.sampleInterval || 10000;
 
-  trans.add({
-    topic,
-    cmd: 'processInfo',
-    pubsub$: true
-  }, function (resp, cb) {
-    const info = {
-      app: trans.config.name,
-      eventLoopDelay: trans.load.eventLoopDelay,
-      heapUsed: trans.load.heapUsed,
-      rss: trans.load.rss,
-      nodeEnv: process.env.NODE_ENV,
-      uptime: process.uptime(),
-      ts: Date.now()
-    };
-    debug('processInfo', info);
-    cb(null, info);
-  });
-
-  trans.add({
-    topic,
-    cmd: 'actionsInfo',
-    pubsub$: true
-  }, function (resp, cb) {
-    const info = {
-      app: trans.config.name
-    };
-
-    const list = trans.list();
-    info.ts = Date.now();
-    info.actions = list.map(action => {
-      const schema = {};
-      return {
-        pattern: action.pattern,
-        plugin: action.plugin.attributes.name
-      };
-    });
-
-    debug('actionsInfo', info);
-    cb(null, info);
-  });
+  pubProcessInfo(trans, topic, sampleInterval);
+  pubActionsInfo(trans, topic, sampleInterval);
 
   next();
 }
