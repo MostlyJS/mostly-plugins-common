0884141fbffcf77207c148fe5073585c8c4139fb
forafish <fora.luo@gmail.com>
2018-04-02 03:10:06 +0800

    add time log for esm loader

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 require = require("esm")(module/*, options*/);
+console.time('mostly-plugins-common import');
 module.exports.stats = require('./src/stats').default;
+console.timeEnd('mostly-plugins-common import');
